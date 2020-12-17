eedab263b41bc55fc9829e7e848a2fb48ca2a487
David M. Weigl <weigl@mdw.ac.at>
2020-12-17 12:26:56 +0100

    Update generate_structure_rdf arguments to match TPL expectations

--- scripts/generate_structure_rdf.sh
+++ scripts/generate_structure_rdf.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash 
-python scripts/convert_to_rdf.py --format $1 --meiFile $2 --meiUri $3 --segmentlineUri $4 --segmentlineOutput $5
+python scripts/convert_to_rdf.py --format jsonld --meiFile $1 --segmentlineOutput $2 --meiUri $3 --segmentlineUri $4
