51f3bfed232d835d9090877986c3492d7792fdba
David M. Weigl <weigl@mdw.ac.at>
2020-12-18 17:21:05 +0100

    change invocation according to TPL requirements

--- scripts/generate_structure_rdf.sh
+++ scripts/generate_structure_rdf.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/bash 
-python scripts/convert_to_rdf.py --format jsonld --meiFile $1 --segmentlineOutput $2 --meiUri $3 --segmentlineUri $4
+export segmentlineHost=$4
+[[ "${segmentlineHost}" != */ ]] && segmentlineHost="${segmentlineHost}/"
+[[ "${segmentlineHost}" == */ ]] && segmentlineHost="${segmentlineHost: : -1}"
+python scripts/convert_to_rdf.py --format jsonld --meiFile $1 --segmentlineOutput $2 --meiUri $3 --segmentlineUri $segmentlineHost/$2
