4563995d96f881ae801ce856ce28e19d0c9989c5
David M. Weigl <weigl@mdw.ac.at>
2021-04-08 16:56:17 +0200

    optionally require a performance midi file OR a stringified performance midi JSON object (but not both)

--- scripts/performance_alignment_workflow.py
+++ scripts/performance_alignment_workflow.py
@@ -4,22 +4,25 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--smatPath', help="Absolute path of locally installed Symbolic Music Alignment Tool folder", required=True)
 parser.add_argument('--meiUri', help="URI of MEI file being performed", required=True)
 parser.add_argument('--structureUri', help="URI of the structural segmentation RDF for the MEI file being performed", required=True)
-parser.add_argument('--performanceMidi', help="Stringified JSON object containing MIDI event data received from client", required=True)
 parser.add_argument('--timelineOutput', help="Path to the alignment timeline JSONLD file to be generated", required=True)
 parser.add_argument('--solidClaraBaseUri', help="URI of base CLARA folder in user's SOLID Pod", required=True)
 parser.add_argument('--tpl-out', help="File used by TROMPA Processing Library to identify the segment RDF output", required=True)
-
+perfMidiGroup = parser.add_mutually_exclusive_group(required=True)
+perfMidiGroup.add_argument('--performanceMidi', help="Stringified JSON object containing MIDI event data received from client")
+perfMidiGroup.add_argument('--performanceMidiFile', help="Locally stored MIDI file for the performance")
 args = parser.parse_args()
 
 tmpUuid = os.path.join(os.getcwd(), "") + str(uuid.uuid4()) + ".tmp."
 
-try: 
-    print("** ALIGNMENT STEP 0: Writing performance MIDI to file")
-    ret = os.system("python {scriptsPath}/midi-events-to-file.py --midiJson {performanceMidi} --output {midiOut}".format(
-            performanceMidi = args.performanceMidi,
-            midiOut = tmpUuid + "performance.mid"
+try:
+    if args.perfMidi is not None:
+        print("** ALIGNMENT STEP 0: Writing performance MIDI to file")
+        ret = os.system("python {scriptsPath}/midi-events-to-file.py --midiJson {performanceMidi} --output {midiOut}".format(
+                performanceMidi = args.performanceMidi,
+                midiOut = tmpUuid + "performance.mid"
+            )
         )
-    )
+
 
     print("** ALIGNMENT STEP 1: Synthesising canonical (score) MIDI")
     ret = os.system("python {scriptsPath}/mei-to-midi.py --meiUri {meiUri} --output {canonicalMidi}".format(
@@ -36,7 +39,7 @@ try:
             scriptsPath = sys.path[0],
             smatPath = args.smatPath,
             canonicalMidi = tmpUuid + "canonical.mid",
-            performanceMidi = tmpUuid + "performance.mid",
+            performanceMidi = args.performanceMidiFile if args.performanceMidiFile is not None else tmpUuid + "performance.mid",
             corresp = tmpUuid + "corresp"
         )
     )
