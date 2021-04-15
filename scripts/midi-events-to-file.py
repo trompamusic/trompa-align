96411d6f9b217076c4c68482ae4081122a4eb378
David M. Weigl <weigl@mdw.ac.at>
2021-04-15 16:13:05 +0200

    more midi generation improvements

--- scripts/midi-events-to-file.py
+++ scripts/midi-events-to-file.py
@@ -5,7 +5,6 @@ from mido import Message, MidiFile, MidiTrack, second2tick, bpm2tempo
 import json
 import argparse
 
-codesToInclude = [64, 144]
 ticks_per_beat = 5000
 tempo = bpm2tempo(120)
 
@@ -20,7 +19,8 @@ if __name__ == "__main__":
     with open(midiJson, 'r') as j:
         midiBatchJson = j.read()
     midiBatch = json.loads(midiBatchJson)
-    midiNotes = list(filter(lambda e: e["data"]["_messageCode"] in codesToInclude, midiBatch))
+#    midiNotes = list(filter(lambda e: e["data"]["_messageCode"] in codesToInclude, midiBatch))
+    midiNotes = midiBatch
     midiFile = MidiFile()
     midiFile.ticks_per_beat = ticks_per_beat
         
@@ -31,21 +31,47 @@ if __name__ == "__main__":
     
     for note in midiNotes:
         # write into MIDI file with seconds2ticks for timestamp
-        eventType = "UNKNOWN"
-        code = note["data"]["_data"]["0"]
-        key = note["data"]["_data"]["1"]
-        velocity = note["data"]["_data"]["2"]
-#        print("timestamp: ", (note["timestamp"] - prevTime) / 1000)
-        time = round(second2tick((note["timestamp"]-prevTime) / 1000, ticks_per_beat=ticks_per_beat, tempo=tempo))
-#        print("time in ticks: ", time)
-        if code == 144:
-            eventType = "note_on"
-        elif code == 64:
-            eventType = "note_off"
-        else:
-            print("UNHANDLED MIDI CODE: ", code)
-        if eventType != "UNKNOWN":
-            track.append(Message(eventType, note=key, velocity=velocity, time=time))
-        prevTime = note["timestamp"]
+        try:
+            eventType = "UNKNOWN"
+            code = (note["data"]["_data"]["0"] >> 4) & 0b00000111
+            channel = note["data"]["_data"]["0"] & 0b00001111
+            key = note["data"]["_data"]["1"]
+            velocity = note["data"]["_data"]["2"] & 0b01111111
+
+            print("code: ", code, "channel: ", channel, "key: ", key, "vel: ", velocity)
+
+            time = round(second2tick((note["timestamp"]-prevTime)/1000, ticks_per_beat=ticks_per_beat, tempo=tempo))
+            if code == 0b001:
+                eventType = "note_on"
+                track.append(Message(eventType, channel=0, note=key, velocity=velocity, time=time))
+                print("APPEND NOTE ON: ", eventType, 0, key, velocity, time)
+                prevTime = note["timestamp"]
+            elif code == 0b000:# or code == 0b0000:
+                eventType = "note_off"
+                track.append(Message(eventType, channel=0, note=key, velocity=velocity, time=time))
+                print("APPEND NOTE OFF: ", eventType, 0, key, velocity, time)
+                prevTime = note["timestamp"]
+            elif code == 0b011:
+                eventType = "control_change"
+                track.append(Message(eventType, channel=0, control=key, value=velocity, time=time))
+                print("APPEND CONTROL CHANGE: ", eventType, 0, key, velocity, time)
+                prevTime = note["timestamp"]
+            elif code == 0b010:
+                eventType = "polytouch"
+                #track.append(Message(eventType, channel=0, note=key, value=velocity, time=time))
+            elif code == 0b101:
+                eventType = "aftertouch"
+                #track.append(Message(eventType, channel=0, value=key, time=time))
+            elif code == 0b110:
+                eventType = "pitchwheel"
+                #track.append(Message(eventType, channel=0, pitch=key, time=time))
+            elif code == 0b111:
+                eventType = "sysex"
+                #track.append(Message(eventType, data=(key, velocity), time=time))
+            else:
+                print("UNKNOWN CODE WAS ", code)
+            print("EVENT TYPE: ", eventType);
+        except ValueError as e:
+            print("Problem with: ", note, e)
     midiFile.save(output)
 
