7c31017928b2f4e61f9185c9bfb815a7f4bcb1c9
David M. Weigl <weigl@mdw.ac.at>
2021-06-12 21:36:02 +0200

    fix bug when midi_to_mp3.py called directly from CLI

--- scripts/midi_to_mp3.py
+++ scripts/midi_to_mp3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from midi2audio import FluidSynth
 from pydub import AudioSegment
+import tempfile
 import argparse, uuid, os 
 
 def midi_to_mp3(midifile, output, tempdir):
@@ -15,5 +16,5 @@ if __name__ == "__main__":
     parser.add_argument('--midiFile', '-m', help="Path to a MIDI file", required=True)
     parser.add_argument('--output', '-o', help="Name of output MP3 file to generate", required=True)
     args = parser.parse_args()
-    midi_to_mp3(args.midiFile, args.output, tmpfile.mkdtemp())
+    midi_to_mp3(args.midiFile, args.output, tempfile.mkdtemp())
 
