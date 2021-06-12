6744c11e084b42de630c31285bca23c76232bfc3
Aggelos Gkiokas <aggelos.gkiokas@upf.edu>
2021-06-12 18:15:54 +0200

    entry point tpl1

--- scripts/tpl_entrypoint_split_1.py
+++ scripts/tpl_entrypoint_split_1.py
@@ -118,14 +118,14 @@ if __name__ == '__main__':
 
     )
   #  performance_data = read_from_solid(args.webId, args.performanceMidiUri, "application/ld+json").json()
-    performance_data = open(args.performanceMidi,'r').read()
+    performance_data = open(args.performance,'r').read()
     performance_data = json.loads(performance_data)
     webmidi_to_midi(performance_data, tempdir)
     mei_to_midi(mei_data, os.path.join(tempdir,"meiMidi.mid"))
     performance_midi = os.path.join(tempdir,"performanceMidi.mid")
     score_midi = os.path.join(tempdir,"meiMidi.mid")
     mei_file = os.path.join(tempdir, "score.mei")
-    audio_fname = args.audioFilename
+    audio_fname = args.audio
     maps_fname = args.maps
 
     main(performance_midi, score_midi, mei_file, audio_fname, tempdir, maps_fname)
