c93e7ddd29fb6f09ffc6e8101665c9e7505ff821
Alastair Porter <alastair@porter.net.nz>
2023-02-28 11:53:35 +0100

    Remove unneeded R packages

--- scripts/install-packages.R
+++ scripts/install-packages.R
@@ -1,1 +1,1 @@
-install.packages(c('httr', 'rvest', 'xml2', 'tidyverse', 'jsonlite', 'fuzzyjoin', 'glue'), repos = "https://cloud.r-project.org/")
+install.packages(c('tidyverse', 'jsonlite', 'fuzzyjoin', 'glue'), repos = "https://cloud.r-project.org/")
