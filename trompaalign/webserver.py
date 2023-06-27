34a282925179e3d2f5738fc0d81de50f224c2413
Alastair Porter <alastair@porter.net.nz>
2023-06-27 20:04:14 +1200

    Align workflow

--- trompaalign/webserver.py
+++ trompaalign/webserver.py
@@ -12,7 +12,8 @@ from trompasolid.backend.db_backend import DBBackend
 from trompaalign import extensions, tasks
 from trompasolid import client
 
-from trompaalign.solid import SolidError, lookup_provider_from_profile
+from trompaalign.solid import SolidError, lookup_provider_from_profile, upload_webmidi_to_pod, upload_midi_to_pod, \
+    get_storage_from_profile
 
 
 def celery_init_app(app: flask.Flask) -> Celery:
@@ -145,15 +146,63 @@ def add_score():
     return jsonify({"status": "queued", "task_id": task.task_id})
 
 
-@webserver_bp.route("/align")
+@webserver_bp.route("/align", methods=["POST"])
 def align():
-    score = request.args.get('score')
-    performance = request.args.get('performance')
-    webid = request.args.get('webid')
-    provider = request.args.get('provider')
+    file = request.files.get('file')
+    payload = file.read()
+    midi_type = request.form.get('midi_type')
+    score_url = request.form.get('score')
+    profile = request.form.get('profile')
 
-    tasks.align.delay(score, performance, provider, webid)
-    return jsonify({"status": "ok"})
+    print("MIDI TYPE", midi_type)
+    print("SCORE URL", score_url)
+    print("PROFILE", profile)
+    #webmidi_url = request.args.get('webmidi_url')
+    #midi_url = request.args.get('midi_url')
+
+    # if webmidi_url and midi_url:
+    #     return jsonify({"status": "error", "message": "Must have only one of webmidi_url or midi_url"}), 400
+
+    provider = lookup_provider_from_profile(profile)
+    storage = get_storage_from_profile(profile)
+
+    print("Uploading file")
+    if midi_type == "webmidi":
+        webmidi_url = upload_webmidi_to_pod(provider, profile, storage, payload)
+        midi_url = None
+    elif midi_type == "midi":
+        midi_url = upload_midi_to_pod(provider, profile, storage, payload)
+        webmidi_url = None
+    else:
+        return jsonify({"status": "error", "message": "Must have midi_type of webmidi or midi"}), 400
+
+    # profile, score_url, webmidi_url, midi_url
+    task = tasks.align_recording.delay(profile, score_url, webmidi_url, midi_url)
+    print("made task", task.task_id)
+    return jsonify({"status": "queued", "task_id": task.task_id})
+
+
+@webserver_bp.route("/align/status")
+def align_status():
+    task_id = request.args.get("task")
+    if not task_id:
+        return jsonify({"status": "error", "message": "Missing `task` parameter"}), 400
+
+    result = AsyncResult(task_id)
+    if result.failed():
+        if isinstance(result.result, SolidError):
+            # This is a known failure mode, one of our custom exceptions
+            return jsonify({"status": "error", "message": str(result.result)})
+        else:
+            # An unknown failure mode
+            return jsonify({"status": "unknown", "message": str(result.result)})
+    else:
+        if result.ready():
+            # Finished
+            return jsonify({"status": "ok", "message": result.result})
+        else:
+            # Still running
+            return jsonify({"status": "pending"})
 
 
 @webserver_bp.route('/', defaults={'path': 'index.html'})
