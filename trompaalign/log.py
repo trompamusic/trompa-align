7f3e933737c0fc94639fb169e6a5865610b52dbf
Alastair Porter <alastair@porter.net.nz>
2026-01-09 12:18:18 +0100

    Remove unused fluent dependency

--- trompaalign/log.py
+++ trompaalign/log.py
@@ -1,29 +1,9 @@
 import logging
-from io import BytesIO
-
-import msgpack
-import fluent.handler
-
-custom_format = {
-    "host": "%(hostname)s",
-    "where": "%(module)s.%(funcName)s",
-    "type": "%(levelname)s",
-    "stack_trace": "%(exc_text)s",
-}
-
-
-def overflow_handler(pendings):
-    unpacker = msgpack.Unpacker(BytesIO(pendings))
-    for unpacked in unpacker:
-        print(unpacked)
-
 
 logger = logging.getLogger("trompaalign")
 
-handler = fluent.handler.FluentHandler(
-    "app.follow", host="fluentd", port=24224, buffer_overflow_handler=overflow_handler
-)
-formatter = fluent.handler.FluentRecordFormatter(custom_format)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(module)s.%(funcName)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
